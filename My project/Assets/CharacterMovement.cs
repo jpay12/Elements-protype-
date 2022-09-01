c34960a0303b35684db60ca19f0888d335ddbc60
jpay12 <97638950+jpay12@users.noreply.github.com>
2022-09-01 01:03:03 -0400

    character script not working correctly yet

--- My project/Assets/CharacterMovement.cs
+++ My project/Assets/CharacterMovement.cs
@@ -4,15 +4,81 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float speed = 5f;
+
+  public float moveSpeed; 
+
+  float horizontalInput;
+
+  float verticalInput;    
+    
+  Vector3 moveDirection;
+
+  private vector 2 currentInput; 
+
+  public Transform orientation; 
+
+  Rigidbody rb; 
+
+  public float maxFuel = 4f;
+
+    public float thrustForce = 0.5f;
+
+ public Rigidbody rigid;
+
+ public Transform groundedTransform;
+
+
+ private float curFuel;
+
+   private void Start()
     {
-        
+      rb = GetComponent<Rigidbody>();
+       curFuel = maxFuel;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        MyInput(); 
+
+        if (Input.GetAxis("Jump") > 0.5f && curFuel > 0.5f)
+        {
+            curFuel -= Time.deltaTime;
+            rigid.AddForce(rigid.transform.up * thrustForce, ForceMode.Impulse);
+            
+        }
+        else if(Physics.Raycast(groundedTransform.position, Vector3.down, 0.05f, LayerMask.GetMask("Grounded")) && curFuel < maxFuel)
+        {
+            curFuel += Time.deltaTime;
+           
+        }
+
     }
+     
+    private void  FixedUpdate()
+    {
+        MovePlayer(); 
+    }
+
+    private void MyInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal"); 
+        verticalInput = Input.GetAxisRaw("Vertical"); 
+    }
+
+    private void MovePlayer()
+    {
+        // calculate movement direction
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        rb.AddForce(moveDirection.normalized * moveSpeed * 5f, ForceMode.Force); 
+   }
+
+   
 }
+
+
+
+
+ 
+
